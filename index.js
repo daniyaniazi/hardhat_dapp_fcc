331f28dd508bf53a020b9c3a7ce6f49f9cbd3865
iamcsstudent123 <farihadania@hotmail.com>
2022-10-20 12:03:50 +0500

    add prettier

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-console.log("Script Working");
+console.log("Script Working")
 
 async function connect() {
-  if (typeof window.ethereum != null) {
-    console.log("I see MetaMask");
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    console.log("Opened Metamask");
-  } else {
-    document.getElementById("info").innerHTML = "Please Install a MetaMask";
-    console.log("No MetaMask");
-  }
+    if (typeof window.ethereum != null) {
+        console.log("I see MetaMask")
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        console.log("Opened Metamask")
+    } else {
+        document.getElementById("info").innerHTML = "Please Install a MetaMask"
+        console.log("No MetaMask")
+    }
 }
+
+async function fund(ethAmount) {}
