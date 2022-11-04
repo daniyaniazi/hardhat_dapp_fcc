b5335daaa4e38c6c9c5904bde9e81aff67f8a7b9
iamcsstudent123 <farihadania@hotmail.com>
2022-11-05 01:44:25 +0500

    header css

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,14 +2,20 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 font-bold text-3xl text-indigo-600">
-                {" "}
-                Decentralized Lottery
-            </h1>
-            <div className="ml-auto py-2 px-4">
-                <ConnectButton moralisAuth={false} />
+        <div class="bg-gray-50">
+            <div class="mx-auto max-w-7xl py-8 px-8 sm:px-4 lg:flex lg:items-center lg:justify-between lg:py-8 lg:px-8">
+                <h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    <span class="block">Ready to dive in?</span>
+                    <span class="block text-indigo-600"> Decentralized Lottery</span>
+                </h2>
+                <div class="flex lg:mt-0 lg:flex-shrink-0">
+                    <div class="ml-3 inline-flex rounded-md shadow">
+                        <div className="ml-auto py-1 px-2">
+                            <ConnectButton moralisAuth={false} />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </nav>
+        </div>
     )
 }
