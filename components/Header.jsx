c8e745faf5fa813384bcac5dc3c5ad59ed698ed6
iamcsstudent123 <farihadania@hotmail.com>
2022-11-05 01:35:48 +0500

    style the application

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,14 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <>
-            <p>Decenetralized Lottery</p>
-            <ConnectButton></ConnectButton>
-        </>
+        <nav className="p-5 border-b-2 flex flex-row">
+            <h1 className="py-4 px-4 font-bold text-3xl text-indigo-600">
+                {" "}
+                Decentralized Lottery
+            </h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
     )
 }
--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -21,7 +21,11 @@ export default function LotteryEntrance() {
         params: {},
     })
 
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -37,6 +41,7 @@ export default function LotteryEntrance() {
     const handleSuccess = async (transaction) => {
         await transaction.wait(1)
         handleNotification(transaction)
+        updateUIValues()
     }
     const handleNotification = async (transaction) => {
         dispatch({
@@ -75,20 +80,71 @@ export default function LotteryEntrance() {
         }
     }, [isWeb3Enabled])
     return (
-        <>
+        <div className="p-5">
             {raffleAddress ? (
-                <div>
-                    <button onClick={enterRaffleLottery}>Enter Raffle</button>
-                    <p>
-                        Entrance Fee{" "}
-                        {entranceFee ? ethers.utils.formatUnits(entranceFee, "ether") : ""} ETH
-                    </p>
-                    <p>No of Players : {noOfPlayers}</p>
-                    <p>Reccent Winner : {recentWiner}</p>
+                <div class="block">
+                    <div className="m-4">
+                        <button
+                            disabled={isLoading || isFetching}
+                            class=" bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={enterRaffleLottery}
+                        >
+                            {isLoading || isFetching ? (
+                                <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                            ) : (
+                                "Enter Raffle"
+                            )}
+                        </button>
+                    </div>
+
+                    <div class="m-4 overflow-hidden bg-white shadow sm:rounded-lg">
+                        <div class="px-4 py-5 sm:px-6">
+                            <h3 class="text-lg font-medium leading-6 text-gray-900">
+                                Raffle Lottery Updates
+                            </h3>
+                            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                                Enter a raffle, get a chance to win!
+                            </p>
+                        </div>
+                        <div class="border-t border-gray-200">
+                            <dl>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        {" "}
+                                        Entrance Fee
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                        {" "}
+                                        {entranceFee
+                                            ? ethers.utils.formatUnits(entranceFee, "ether")
+                                            : ""}{" "}
+                                        ETH
+                                    </dd>
+                                </div>
+                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        No of Players
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                        {" "}
+                                        {noOfPlayers}
+                                    </dd>
+                                </div>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        Reccent Winner
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                        {recentWiner}
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
                 </div>
             ) : (
                 <div>No Raffle Address</div>
             )}
-        </>
+        </div>
     )
 }
