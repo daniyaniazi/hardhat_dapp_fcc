2e9aa710d87f4d4b7ec2bf3356379194e43b5eae
iamcsstudent123 <farihadania@hotmail.com>
2022-11-05 17:58:52 +0500

    fix errors

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,15 +2,15 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div class="bg-gray-50">
-            <div class="mx-auto max-w-7xl py-8 px-8 sm:px-4 lg:flex lg:items-center lg:justify-between lg:py-8 lg:px-8">
-                <h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    <span class="block">Ready to dive in?</span>
-                    <span class="block text-indigo-600"> Decentralized Lottery</span>
+        <div classNameName="bg-gray-50">
+            <div className="mx-auto max-w-7xl py-8 px-8 sm:px-4 lg:flex lg:items-center lg:justify-between lg:py-8 lg:px-8">
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    <span className="block">Ready to dive in?</span>
+                    <span className="block text-indigo-600"> Decentralized Lottery</span>
                 </h2>
-                <div class="flex lg:mt-0 lg:flex-shrink-0">
-                    <div class="ml-3 inline-flex rounded-md shadow">
-                        <div className="ml-auto py-1 px-2">
+                <div className="flex lg:mt-0 lg:flex-shrink-0">
+                    <div className="ml-3 inline-flex rounded-md shadow">
+                        <div classNameName="ml-auto py-1 px-2">
                             <ConnectButton moralisAuth={false} />
                         </div>
                     </div>
--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -82,11 +82,11 @@ export default function LotteryEntrance() {
     return (
         <div className="p-5">
             {raffleAddress ? (
-                <div class="block">
+                <div className="block">
                     <div className="m-4">
                         <button
                             disabled={isLoading || isFetching}
-                            class=" bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            className=" bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                             onClick={enterRaffleLottery}
                         >
                             {isLoading || isFetching ? (
@@ -97,23 +97,23 @@ export default function LotteryEntrance() {
                         </button>
                     </div>
 
-                    <div class="m-4 overflow-hidden bg-white shadow sm:rounded-lg">
-                        <div class="px-4 py-5 sm:px-6">
-                            <h3 class="text-lg font-medium leading-6 text-gray-900">
+                    <div className="m-4 overflow-hidden bg-white shadow sm:rounded-lg">
+                        <div className="px-4 py-5 sm:px-6">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">
                                 Raffle Lottery Updates
                             </h3>
-                            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                            <p className="mt-1 max-w-2xl text-sm text-gray-500">
                                 Enter a raffle, get a chance to win!
                             </p>
                         </div>
-                        <div class="border-t border-gray-200">
+                        <div className="border-t border-gray-200">
                             <dl>
-                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
                                         {" "}
                                         Entrance Fee
                                     </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                         {" "}
                                         {entranceFee
                                             ? ethers.utils.formatUnits(entranceFee, "ether")
@@ -121,20 +121,20 @@ export default function LotteryEntrance() {
                                         ETH
                                     </dd>
                                 </div>
-                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
                                         No of Players
                                     </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                         {" "}
                                         {noOfPlayers}
                                     </dd>
                                 </div>
-                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500">
                                         Reccent Winner
                                     </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                         {recentWiner}
                                     </dd>
                                 </div>
